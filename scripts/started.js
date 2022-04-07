4366296133fef74d2d6c4610d6d56c2c6ac27564
Phillip Cabrera <pcabreram1234@gmail.com>
2022-04-06 21:03:29 -0400

    initialConfig.json modified

--- scripts/started.js
+++ scripts/started.js
@@ -1,10 +1,9 @@
 const fs = require("fs-extra");
 
-const reg = //
-  fs.copy("../src/assets/static/[name].png", "../build/assets", (err) => {
-    if (err) return console.error(err);
-    console.log("success!");
-  }); // copies directory, even if it has subdirectories or files
+fs.copy("../src/assets/static/", "../build/assets", (err) => {
+  if (err) return console.error(err);
+  console.log("success!");
+}); // copies directory, even if it has subdirectories or files
 
 /* fs.copyFile(
   path.resolve(__dirname, "..", "src", "assets", "static"),
