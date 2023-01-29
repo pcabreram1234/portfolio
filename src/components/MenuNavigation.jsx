4c1e5fb70fd84d82be4d7129e210cf4d731f4aaa
Phillip Cabrera <pcabreram1234@gmail.com>
2023-01-28 21:27:45 -0400

    Menu componente modified and CSS

--- src/components/MenuNavigation.jsx
+++ src/components/MenuNavigation.jsx
@@ -1,61 +1,47 @@
 import React from "react";
-import { Menu } from "antd";
-import {
-  HomeFilled,
-  InfoCircleFilled,
-  ProjectFilled,
-  ContactsFilled,
-} from "@ant-design/icons";
 import "../styles/MenuNavigation.css";
 const MenuNavigation = () => {
-  const menuItems = [
-    {
-      label: (
-        <a href="#home" style={{ color: "white" }}>
-          Home
-        </a>
-      ),
-      key: "home",
-      icon: <HomeFilled style={{ color: "white" }} />,
-    },
-    {
-      label: (
-        <a href="#about" style={{ color: "white" }}>
-          About
-        </a>
-      ),
-      key: "about",
-      icon: <InfoCircleFilled style={{ color: "white" }} />,
-    },
-    {
-      label: (
-        <a href="#projects" style={{ color: "white" }}>
-          Projects
-        </a>
-      ),
-      key: "projects",
-      icon: <ProjectFilled style={{ color: "white" }} />,
-    },
-    {
-      label: (
-        <a href="#contacts" style={{ color: "white" }}>
-          Contacts
-        </a>
-      ),
-      key: "contacts",
-      icon: <ContactsFilled style={{ color: "white" }} />,
-    },
-  ];
   return (
-    <Menu
-      items={menuItems}
-      mode="horizontal"
+    <div
       style={{
-        justifyContent: "flex-end",
-        background: "transparent",
-        borderColor: "transparent",
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        width: "100%",
       }}
-    ></Menu>
+      className="menuNav"
+    >
+      <ul
+        style={{
+          listStyle: "none",
+          display: "flex",
+
+          gridColumn: "2 / 3",
+          justifyContent: "space-around",
+        }}
+        className="menuNav-list"
+      >
+        <li>
+          <a style={{ color: "white" }} href="#home">
+            Home
+          </a>
+        </li>
+        <li>
+          <a style={{ color: "white" }} href="#about">
+            About
+          </a>
+        </li>
+        <li>
+          <a style={{ color: "white" }} href="#projects">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a style={{ color: "white" }} href="#contacts">
+            Contacts
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
